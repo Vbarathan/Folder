50f53f19fb240cbb8cf8d10967560aca0533d486
Barathan.V Bsc.cs <144104255+Vbarathan@users.noreply.github.com>
2023-09-21 13:06:02 +0530

    Challenge-3.1.py

--- Challenge-3.1.py
+++ Challenge-3.1.py
@@ -1,9 +1,4 @@
-"""
-Write a function called linear_search_product that takes the list of products and a target product
-name as input. The function should perform a linear search to find the target product in the list and
-return a list of indices of all occurrences of the product if found, or an empty list if the product is not
-found.
-"""
+
 
 
 def linearSearchProduct(productList, targetProduct):
@@ -16,7 +11,7 @@ def linearSearchProduct(productList, targetProduct):
   return indices
 
 
-# Example usage:
+
 products = ["shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
 target = "shoes"
 target2 = 'apple'
